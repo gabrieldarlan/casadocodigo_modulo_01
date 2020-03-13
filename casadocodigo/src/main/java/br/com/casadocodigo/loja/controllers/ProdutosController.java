88062286d5dc0ba0b7782534c266ab89320aa26b
gabrieldarlan <gabriel.darlan88@gmail.com>
2020-03-13 14:30:20 -0300

    aula 04 - 02 Preparando Controller, DAO e JSP

--- casadocodigo/src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
@@ -1,8 +1,11 @@
 package br.com.casadocodigo.loja.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.casadocodigo.loja.daos.ProdutoDao;
@@ -19,14 +22,22 @@ public class ProdutosController {
 	public ModelAndView form() {
 		ModelAndView modelAndView = new ModelAndView("produtos/form");
 		modelAndView.addObject("tipos", TipoPreco.values());
-		
+
 		return modelAndView;
 	}
 
-	@RequestMapping("/produtos")
+	@RequestMapping(value = "/produtos", method = RequestMethod.POST)
 	public String grava(Produto produto) {
 		System.out.println(produto);
 		produtoDao.gravar(produto);
 		return "produtos/ok";
+	}
+
+	@RequestMapping(value = "/produtos", method = RequestMethod.GET)
+	public ModelAndView listar() {
+		List<Produto> produtos = produtoDao.listar();
+		ModelAndView modelAndView = new ModelAndView("/produtos/lista");
+		modelAndView.addObject("produtos", produtos);
+		return modelAndView;
 	}
 }
