f6c580ac492d471882d1b9335207164e252d1a26
gabrieldarlan <gabriel.darlan88@gmail.com>
2020-03-16 15:00:57 -0300

    aula 04 - 04 Criando Usuario e UsuarioDAO a partir do minuto 14:05

--- casadocodigo/src/main/java/br/com/casadocodigo/loja/conf/SecurityConfiguration.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/conf/SecurityConfiguration.java
@@ -1,12 +1,22 @@
 package br.com.casadocodigo.loja.conf;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import br.com.casadocodigo.loja.dao.UsuarioDao;
 
 @EnableWebMvcSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+
+	@Autowired
+	private UsuarioDao usuarioDao;
+
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -22,5 +32,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 		    .anyRequest().authenticated()
 		    .and().formLogin();
 	}
+
 	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(usuarioDao)
+			.passwordEncoder(new BCryptPasswordEncoder());
+	}
 }
