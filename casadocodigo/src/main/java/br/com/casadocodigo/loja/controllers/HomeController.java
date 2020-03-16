203410ba89511e10ef91aa806e27def6cba7fd32
gabrieldarlan <gabriel.darlan88@gmail.com>
2020-03-15 21:27:31 -0300

    aula 01 - 05 Invalidando o cache

--- casadocodigo/src/main/java/br/com/casadocodigo/loja/controllers/HomeController.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/controllers/HomeController.java
@@ -18,7 +18,7 @@ public class HomeController {
 	private ProdutoDao produtoDao;
 
 	@RequestMapping("/")
-	@Cacheable(value = "produtoHome")
+	@Cacheable(value = "produtosHome")
 	public ModelAndView index() {
 		List<Produto> produtos = produtoDao.listar();
 		ModelAndView modelAndView = new ModelAndView("home");
